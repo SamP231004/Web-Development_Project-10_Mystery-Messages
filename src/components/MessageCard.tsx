9e37e78ef4f8781eb8700da18dae03b3079bdc66
Samarth Patel <samp231004@gmail.com>
2025-05-22 19:54:58 +0530

    added navbar and dashboard

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import React from 'react';
+import axios, { AxiosError } from 'axios';
+import dayjs from 'dayjs';
+import { X } from 'lucide-react';
+import { toast } from 'sonner';
+import { Message } from '@/model/user.models';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+import { Button } from './ui/button';
+import { ApiResponse } from '@/types/ApiResponse';
+
+type MessageCardProps = {
+    message: Message;
+    onMessageDelete: (messageId: string) => void;
+};
+
+export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
+    const handleDeleteConfirm = async () => {
+        try {
+            const response = await axios.delete<ApiResponse>(
+                `/api/delete-message/${message._id}`
+            );
+            toast.success(response.data.message);
+            if (typeof message._id === 'string') {
+                onMessageDelete(message._id);
+            }
+            else {
+                toast.error('🙁 Hmm… that message ID isn’t valid');
+            }
+        } 
+        catch (error) {
+            const axiosError = error as AxiosError<ApiResponse>;
+            toast.error(
+                axiosError.response?.data.message ?? '⚠️ Message deletion failed.'
+            );
+        }
+    };
+
+    return (
+        <Card className="card-bordered">
+            <CardHeader>
+                <div className="flex justify-between items-center">
+                    <CardTitle>{message.content}</CardTitle>
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="destructive">
+                                <X className="w-5 h-5" />
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>😬 Double-check — are you sure?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    😬 Be careful! This will permanently delete the message.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction onClick={handleDeleteConfirm}>
+                                    Continue
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
+                <div className="text-sm">
+                    {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+                </div>
+            </CardHeader>
+            <CardContent></CardContent>
+        </Card>
+    );
+}
